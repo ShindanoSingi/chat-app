50b179fa6aaee72283bb1c8123eeb4f8ef22858a
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-04-14 00:58:42 -0400

    added cloudinary

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authMiddleware = require('../middlewares/authMiddleware');
 const { response } = require('express');
+const cloudinary = require('../cloudinary');
 
 // User registration
 router.post('/register', async (req, res) => {
@@ -95,6 +96,37 @@ router.get('/get-all-users', authMiddleware, async (req, res) => {
                message: 'Users fetched successfully!',
                data: allUsers,
           });
+     } catch (error) {
+          res.send({
+               message: error.message,
+               success: false,
+          });
+     }
+});
+
+// Update user profile picture
+router.post('/update-profile-picture', authMiddleware, async (req, res) => {
+     try {
+          const image = req.body.image;
+
+          // Upload image to cloudinary and get the url
+          const uploadedImage = await cloudinary.uploader.upload(image, {
+               upload_preset: 'bembe-chat',
+          });
+
+          // Update user profile picture
+          const user = await User.findOneAndUpdate(
+               { _id: req.body.userId },
+               { profilePic: uploadedImage.secure_url },
+               { new: true }
+          );
+
+          res.send({
+               success: true,
+               message: 'Profile picture updated successfully!',
+               data: user,
+          });
+
      } catch (error) {
           res.send({
                message: error.message,
