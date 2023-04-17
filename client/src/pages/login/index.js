829045cb31ea479d28666900066dcfd56bf93873
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-04-16 20:57:14 -0400

    Login mobile responsive completed

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { LoginUser } from '../../apicalls/users';
 import { showLoader, hideLoader } from '../../redux/loaderSlice';
+import './index.css'
 
 
 function Login() {
@@ -49,11 +50,11 @@ function Login() {
      //      });
 
      return (
-          <div className='h-screen bg-primary flex items-center  justify-center'>
-               <div className='bg-white shadow-md p-5 flex flex-col gap-5 w-96'>
+          <div className='min-h-screen py-5 bg-primary flex items-center  justify-center'>
+               <div className='form-container bg-white shadow-md p-5 flex flex-col gap-5 w-96'>
                     <div className='flex justify-center gap-2 items-center'>
-                         <i className="ri-wechat-fill text-3xl text-primary"></i>
-                         <h1 className="text-2xl uppercase text-center font-semibold text-primary">Bembe-Chat Login</h1>
+                         <i className="ri-wechat-fill text-3xl text-primary chat-icon"></i>
+                         <h1 className="text-2xl uppercase text-center font-semibold text-primary login-title">Bembe-Chat Login</h1>
                     </div>
 
                     <hr />
@@ -69,9 +70,9 @@ function Login() {
                          placeholder="Enter your password"
                          className={user.email ? 'border-green-500' : 'border-red-500'}
                     />
-                    <button className={user.email && user.password ? 'contained-btn' : 'disabled-btn'} onClick={loginUser}>Login</button>
+                    <button className={user.email && user.password ? 'contained-btn login-button' : 'disabled-btn login-button'} onClick={loginUser}>Login</button>
                     <Link to='/register'
-                         className='underline text-center'
+                         className='underline text-center login-link'
                     >
                          Don't have an account? Register
                     </Link>
