99ca92c2ae3c16682452e2b9c20886b494e4a03d
Shindano Singi <shindanosingi@Shindanos-Laptop.local>
2023-04-23 04:25:06 -0400

    trying to deploy using build used audit fixed1

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ const path = require('path');
 __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build')));
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
 }
